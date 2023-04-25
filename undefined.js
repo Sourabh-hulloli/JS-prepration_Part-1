d018a9b9538d7431329a5d9f7fdf381c82b2f0ab
sourabh hulloli <sourabh.hulloli@gmail.com>
2023-04-25 23:58:16 +0530

    learned undefined, null, BigInt

--- undefined.js
+++ undefined.js
@@ -1,5 +1,29 @@
 // undefined
 
+let userName;
+console.log(typeof userName); // undefined
+
+userName = "Sourabh";
+console.log(typeof userName); // String
+// const friendname;
+
 // null
 
+let myVariable = null;
+
+console.log(myVariable);
+console.log(typeof myVariable);
+
 // BigInt
+let myNumber = BigInt(9383747849843307092408423728);
+console.log(myNumber);
+
+// OR we can also write BigInt in below example
+
+let newNumber = 123n;
+console.log(newNumber);
+
+const largeNumber = 12534n;
+const normalNumber = 10;
+
+console.log(largeNumber + normalNumber); // throws error
