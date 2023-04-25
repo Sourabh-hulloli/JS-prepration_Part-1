55e475ce675c398f634d46bbdb86da5e2560168c
sourabh hulloli <sourabh.hulloli@gmail.com>
2023-04-25 16:03:06 +0530

    prepration on basic JS

---
+++ variables.js
@@ -0,0 +1,11 @@
+// intro to variables
+
+// variables can store some information.
+// we can use that information later.
+// we can change that information later.'
+
+// declare a variable
+
+// use a variable
+
+// change value
