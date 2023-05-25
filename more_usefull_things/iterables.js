54ae7d7b9cad4336850a8416e1717e8f7c5f5579
sourabh hulloli <sourabh.hulloli@gmail.com>
2023-05-25 12:34:57 +0530

    important array methods completd

---
+++ more_usefull_things/iterables.js
@@ -0,0 +1,8 @@
+// iterables
+// iterables on which we can apply loops
+// string, array are iterable
+
+// array like object
+// array like object have length property
+// and we access by index
+// ex : - string.
