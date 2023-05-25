40d275f4da8a4c48e1fe532f7f2e58ad58fbae7b
sourabh hulloli <sourabh.hulloli@gmail.com>
2023-05-25 21:44:04 +0530

    object cloning

--- more_usefull_things/iterables.js
+++ more_usefull_things/iterables.js
@@ -6,3 +6,22 @@
 // array like object have length property
 // and we access by index
 // ex : - string.
+
+// string
+const firstName = "Sourabh";
+for (let char of firstName) {
+  console.log(char);
+}
+
+// array
+const items = ["item1", "item2", "item3"];
+for (let item of items) {
+  console.log(item);
+}
+
+// object -- object is not iterable.
+// const products = { productId: 1, productName: "Mobile", price: 30000 };
+
+// for (let product of products) {
+//   console.log(product);
+// }
